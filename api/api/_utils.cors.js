a58215f3ac2c0f0dd91756e665459b702a262364
mxplantae <80371957+mxplantae@users.noreply.github.com>
2025-08-26 18:57:55 -0600

    Update _utils.cors.js

--- api/api/_utils.cors.js
+++ api/api/_utils.cors.js
@@ -1,23 +1,21 @@
-// api/_utils.cors.js
-const ALLOWED = new Set([
-  "https://mxplantae.com",
-  "https://www.mxplantae.com"
+export const ALLOWED = new Set([
+  "https://www.mxplantae.com",
+  "https://mxplantae.com"
 ]);
 
 export function withCORS(req, res) {
   const origin = req.headers.origin || "";
   if (ALLOWED.has(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Vary", "Origin");
-  } else {
-    res.setHeader("Vary", "Origin");
   }
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Vary", "Origin");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
 }
 
 export function handlePreflight(req, res) {
   if (req.method === "OPTIONS") {
+    withCORS(req, res);
     res.status(204).end();
     return true;
   }
